c173bf7f3fe33005f5e7d769a84ed95aa246bb83
Bruno Farina <brunofarinadev@yahoo.com.br>
2013-12-18 11:05:41 -0200

    Update server.rb

--- server.rb
+++ server.rb
@@ -1,33 +1,75 @@
+require 'sinatra'
+require 'sinatra/base'
+require 'mongo'
+require 'json/ext'
 
-  require 'sinatra'
-  require 'json'
+include Mongo
 
-  use Rack::Logger
+before do
+	content_type :json    
+	headers 'Access-Control-Allow-Origin' => '*', 
+				 'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST'],
+				 'Access-Control-Allow-Headers' => 'Content-Type'  
+end
 
-  before do
-     content_type :json    
-     headers 'Access-Control-Allow-Origin' => '*', 
-  		   'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST'],
-  		   'Access-Control-Allow-Headers' => 'Content-Type'  
-  end
+set :protection, false
 
-  set :protection, false
+# connecting to the database and so on..
+conn = Mongo::Connection.new
+db   = conn['webdb']
+coll = db['users']				
 
-  options '/movie' do
-  	200
-  end
 
-  get '/movie' do
-    { result: "Monster University" }.to_json
-  end
+#get a list of users...
+get '/users'  do
+  content_type :json
+  headers 'Access-Control-Allow-Origin' => '*', 
+          'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST'],
+          'Access-Control-Allow-Headers' => 'Content-Type'  
+  coll.find.to_a.to_json
+end
 
-  post '/movie' do
+options '/users' do
+	200
+end
 
-    begin
-      params.merge! JSON.parse(request.env["rack.input"].read)
-    rescue JSON::ParserError
-      logger.error "Cannot parse request body." 
-    end
+get '/' do
+	redirect '/users'
+end
 
-    { result: params[:movie], seen: true }.to_json
-  end
+#get a user by ID
+get '/user/:id'  do
+  content_type :json
+  coll.find_one(:_id => params[:id].to_i).to_json
+end
+
+#update a user by ID
+put '/user/:id' do
+  content_type :json
+  id = params[:_id].to_i
+  coll.update({:_id => id}, {"$set" => {
+									:name    => params[:name],
+									:address => params[:address],
+									:city    => params[:city],
+									:country => params[:country],		
+									:email   => params[:email],
+									:phone   => params[:phone].to_i
+									}})
+  coll.find_one(:_id => id).to_json
+end
+
+#post a new users
+post '/user' do
+  id = params[:_id].to_i
+  coll.insert(
+	{
+		:_id     => id,
+		:name    => params[:name],
+		:address => params[:address],
+		:city    => params[:city],
+		:country => params[:country],		
+		:email   => params[:email],
+		:phone   => params[:phone].to_i
+	})
+	coll.find_one(:_id => id).to_json
+end
