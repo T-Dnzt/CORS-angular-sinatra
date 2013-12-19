499648a987184daf648a16a9233134d5aa55de23
Bruno Farina <brunofarinadev@yahoo.com.br>
2013-12-19 08:57:08 -0200

    Update server.rb
    Code improvements.

--- server.rb
+++ server.rb
@@ -6,70 +6,65 @@ require 'json/ext'
 include Mongo
 
 before do
-	content_type :json    
-	headers 'Access-Control-Allow-Origin' => '*', 
-				 'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST'],
-				 'Access-Control-Allow-Headers' => 'Content-Type'  
+  content_type :json    
+  headers 'Access-Control-Allow-Origin' => '*', 
+	  'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST'],
+	  'Access-Control-Allow-Headers' => 'Content-Type'  
 end
 
 set :protection, false
 
 # connecting to the database and so on..
 conn = Mongo::Connection.new
+# set database..
 db   = conn['webdb']
+# set the collection..
 coll = db['users']				
 
 
 #get a list of users...
 get '/users'  do
-  content_type :json
-  headers 'Access-Control-Allow-Origin' => '*', 
-          'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST'],
-          'Access-Control-Allow-Headers' => 'Content-Type'  
   coll.find.to_a.to_json
 end
 
 options '/users' do
-	200
+  200
 end
 
 get '/' do
-	redirect '/users'
+  redirect '/users'
 end
 
-#get a user by ID
+# get a user by ID
 get '/user/:id'  do
-  content_type :json
   coll.find_one(:_id => params[:id].to_i).to_json
 end
 
-#update a user by ID
+# update a user by ID
 put '/user/:id' do
-  content_type :json
   id = params[:_id].to_i
   coll.update({:_id => id}, {"$set" => {
-									:name    => params[:name],
-									:address => params[:address],
-									:city    => params[:city],
-									:country => params[:country],		
-									:email   => params[:email],
-									:phone   => params[:phone].to_i
-									}})
+				:name    => params[:name],
+				:address => params[:address],
+				:city    => params[:city],
+				:country => params[:country],		
+				:email   => params[:email],
+				:phone   => params[:phone].to_i
+				}})
   coll.find_one(:_id => id).to_json
 end
 
-#post a new users
+# post a new users
 post '/user' do
   id = params[:_id].to_i
-  coll.insert(
-	{
-		:_id     => id,
-		:name    => params[:name],
-		:address => params[:address],
-		:city    => params[:city],
-		:country => params[:country],		
-		:email   => params[:email],
-		:phone   => params[:phone].to_i
-	})
-	coll.find_one(:_id => id).to_json
+  coll.insert({
+	:_id     => id,
+	:name    => params[:name],
+	:address => params[:address],
+	:city    => params[:city],
+	:country => params[:country],		
+	:email   => params[:email],
+	:phone   => params[:phone].to_i
+  })
+  coll.find_one(:_id => id).to_json
 end
